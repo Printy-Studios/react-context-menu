a784f6779eec6c2d2864b83bd8800f095aa52aaf
JorensM <jorensmerenjanu@gmail.com>
2023-08-08 10:26:27 +0300

    added style, removed comments and cleaned up code

--- src/ContextMenuContainer.tsx
+++ src/ContextMenuContainer.tsx
@@ -1,4 +1,15 @@
-import React, { HTMLAttributes, PropsWithChildren, ReactNode, useEffect, useRef, useState } from 'react'
+import 
+    React, 
+{ 
+    HTMLAttributes, 
+    PropsWithChildren, 
+    ReactNode, 
+    useEffect, 
+    useRef, 
+    useState 
+} from 'react'
+
+import './ContextMenuContainer.css'
 
 type Position = {
     top?: number | string,
@@ -15,7 +26,6 @@ export default function ContextMenuContainer( { children, menu, className }: Pro
 
     const [showMenu, setShowMenu] = useState<boolean>(false)
     const [menuPosition, setMenuPosition] = useState<Position>({ top: 0, left: 0 })
-    //const [clientPos, setClientPos] = useState({x: 0, y: 0})
 
     const menuRef = useRef<HTMLDivElement>(null)
 
@@ -32,58 +42,44 @@ export default function ContextMenuContainer( { children, menu, className }: Pro
         const final_pos: Position = {}
 
         if (mouse_x + menu_rect.width > window.innerWidth) {
-            console.log('right side')
             final_pos.right = window.innerWidth - mouse_x;
         } else {
             final_pos.left = mouse_x + 1;
         }
 
         if (mouse_y + menu_rect.height > window.innerHeight) {
-            console.log('bottom side')
             final_pos.bottom = window.innerHeight - mouse_y;
         } else {
             final_pos.top = mouse_y + 1;
         }
-
-        console.log('final pos: ', final_pos)
 
         setMenuPosition(final_pos)
         
     }
 
     useEffect(() => {
-        //console.log('changed')
         if (showMenu) {
             menuRef.current.focus()
 
-            
-        } else {
-            // if( !menuRect ) {
-            //     return
-            // }
-
-            
         }
     }, [showMenu])
 
     useEffect(() => {
         menuRef.current.addEventListener('blur', (e: FocusEvent) => {
-            //console.log('blurred')
             setShowMenu(false)
         })
     }, [])
 
     return (
         <div
-            className={className}
+            className={`${className ? ' ' + className : ''}`}
             onContextMenu={handleRightClick}
         >
             <div 
                 ref={menuRef}
                 tabIndex={0}
-                className='absolute focus-reset'
+                className="MenuWrapper"
                 style={{
-                    // height: 'fit-content',
                     visibility: showMenu ? 'visible' : 'hidden',
                     ...menuPosition
                 }}
