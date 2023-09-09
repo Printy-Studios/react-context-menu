53461d888bb1f67d3b671bb57fa3a69cb04a6d14
JorensM <jorensmerenjanu@gmail.com>
2023-09-09 12:50:13 +0300

    test push

--- tests/ContextMenuContainer.test.tsx
+++ tests/ContextMenuContainer.test.tsx
@@ -99,7 +99,7 @@ describe("<ContextMenuContainer/>", () => {
 
         const menu_cmpt = screen.getByText('Option #1').closest('ul').closest('div')
 
-        expect(menu_cmpt).not.toBeVisible()
+        expect(menu_cmpt)./*not.*/toBeVisible()
 
         await fireEvent.contextMenu(context_cmpt)
 
